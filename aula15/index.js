add960da4f2dc3877ae88f25388e35179c7f1531
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2023-06-12 13:23:46 -0300

    commit

---
+++ aula15/index.js
@@ -0,0 +1,25 @@
+let num1 = 9.543256;
+//let num2 = Math.floor(num1); //arrendondado pra baixo
+let num2 = Math.ceil(num1); // arrendonda pra cima
+console.log(num2);
+
+console.log();
+num1 = 9.53466
+num2 = Math.round(num1);// arredonda pro mais proximo
+
+console.log();
+console.log(Math.max(1, 2, 3, 4, 5, 6, 10, -20, -10));
+console.log(Math.min(1, 2, 34, 5, 66, 789));
+console.log();
+const aleatorio = Math.round(Math.random());
+console.log(aleatorio);
+console.log(Math.PI);
+console.log();
+console.log(Math.pow(2, 10)); // 2 ** 10
+console.log();
+num1 = 9;
+console.log(num1 ** (1 / 2));// raiz quadrada
+console.log(num1 ** 0.5); // raiz quadrada
+console.log();
+console.log(100 / 0); // da infinity
+
