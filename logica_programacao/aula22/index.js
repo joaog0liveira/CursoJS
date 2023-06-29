3c24905b5e966d72ecb5170d7020e303dfc85548
João Gabriel <118997962+joaog0liveira@users.noreply.github.com>
2023-06-29 12:39:27 -0300

    commit

---
+++ logica_programacao/aula22/index.js
@@ -0,0 +1,14 @@
+/*
+Operadores lógicos
+&& -> AND  == todas as expressões precisam ser verdadeiras para retornar true 
+|| -> OR  == uma das expressões precisa ser true para retornar true
+! -> NOT == inverso da expressão
+*/
+console.log(true && true); 
+console.log(true && false);
+console.log();
+console.log(false || true); 
+console.log(false || false);
+console.log();
+console.log(!false); // vira true
+console.log(!true); // vira false
